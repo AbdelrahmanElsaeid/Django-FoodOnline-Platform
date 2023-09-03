e3e27d36f9ee38644d13e7056325102115fa6593
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2023-09-03 16:39:56 +0300

    add postgresql database settings

--- project/settings.py
+++ project/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodonline_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'PORT': '5433',
+        'HOST':'localhost',
     }
 }
 
