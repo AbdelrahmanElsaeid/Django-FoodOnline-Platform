497d7732fafdacb6adee0a6005565feb4b94cb99
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2023-09-20 16:29:06 +0300

    modifiy pages&url

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', index, name="index"),
-    path('accounts/', include('accounts.urls'))
+    path('', include('accounts.urls'))
 ]
 
 #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
