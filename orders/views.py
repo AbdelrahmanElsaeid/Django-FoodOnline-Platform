dd63512cb71b1eeb1905550379eb98de13ed6eaa
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2023-10-08 23:29:31 +0300

    add send order confirm& received to customer&vendor

--- orders/views.py
+++ orders/views.py
@@ -7,6 +7,7 @@ from marketplace.context_processor import get_cart_amounts
 from .forms import OrderForm
 from .models import Order, OrderedFood, Payment
 import simplejson as json
+from accounts.utils import send_notification
 # Create your views here.
 
 
@@ -81,4 +82,74 @@ def payments(request):
         order.payment=payment
         order.is_ordered=True
         order.save()
+
+        #move order to order food
+        cart_items =Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            ordered_food = OrderedFood()
+            ordered_food.order = order
+            ordered_food.payment = payment
+            ordered_food.user = request.user
+            ordered_food.fooditem = item.fooditem
+            ordered_food.quantity = item.quantity
+            ordered_food.price = item.fooditem.price
+            ordered_food.amount = item.fooditem.price * item.quantity
+            ordered_food.save()
+        # SEND ORDER CONFIRMATION EMAIL TO THE CUSTOMER
+        mail_subject = 'Thank you for ordering with us.'
+        mail_template = 'orders/order_confirmation_email.html'
+
+    
+        context = {
+            'user': request.user,
+            'order': order,
+            'to_email': order.email,
+            
+        }
+        send_notification(mail_subject, mail_template, context)
+
+
+        #send order received  email  to  the  vendor
+
+
+        mail_subject = 'You Have Received A New Order.'
+        mail_template = 'orders/new_order_received.html'
+
+        to_email=[]
+        for i in cart_items:
+            if i.fooditem.vendor.user.email not in to_email:
+                to_email.append(i.fooditem.vendor.user.email)
+
+    
+        context = {
+            
+            'order': order,
+            'to_email': to_email,
+            
+        }
+        send_notification(mail_subject, mail_template, context)
+        response={
+            'order_number': order_number,
+            'transaction_id':transaction_id
+        }
+        return JsonResponse(response)    
     return HttpResponse('test')
+
+
+
+
+def order_complete(request):
+
+    order_number = request.GET.get('order_number')
+    transaction_id = request.GET.get('transaction_id')
+    try:
+        order = Order.objects.get(order_number=order_number, Payment__transaction_id= transaction_id, is_ordered = True)
+
+        ordered_food = OrderedFood.objects.get(order=order)
+        context = {
+            'order': order,
+            'ordered_food':ordered_food,
+        }
+        return render(request,'orders/order_complete.html', context)
+    except:
+        return redirect('home')
